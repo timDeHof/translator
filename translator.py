91f4da8bc17f07fba781cb9496abda044cd54cad
timDeHof <this.is.tim.dehof@gmail.com>
2022-10-24 21:51:02 -0400

    initial commit

---
+++ translator.py
@@ -0,0 +1,10 @@
+from translate import Translator
+translator = Translator(to_lang="ja")
+try:
+  with open("test", "r") as my_file:
+    text = my_file.read()
+    translation = translator.translate(text)
+    with open('test-ja.txt', 'w') as my_file2:
+        my_file2.write(translation)
+except FileNotFoundError as e:
+    print('check your file path silly!')
